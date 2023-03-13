89ae8c42b9a3792ff9101d3b7b940b316acd1bdb
Anthony Sharp <asharp314@outlook.com>
2023-03-12 22:44:09 -0500

    remove Null Safty, update url call, update resst

--- weather/lib/currentWeather.dart
+++ weather/lib/currentWeather.dart
@@ -14,45 +14,70 @@ class _CurrentWeatherPageState extends State<CurrentWeatherPage> {
     return Scaffold(
       body: Center(
           child: FutureBuilder(
-        builder: (context, snapshot) {
-          if (snapshot != null) {
-            Weather _weather = snapshot.data;
-            if (_weather == null) {
-              return Text("Error getting weather");
-            } else {
-              return weatherBox(_weather);
-            }
-          } else {
-            return CircularProgressIndicator();
-          }
-        },
-        future: getCurrentWeather(),
-      )),
+            builder: (context, snapshot) {
+              if (snapshot != null) {
+
+                // Removing the Null Safty by adding ? in front of the class
+                Weather? _weather = snapshot.data;
+
+                if (_weather == null) {
+                  return Text("Error getting weather");
+                } else {
+                  return weatherBox(_weather);
+                }
+              } else {
+                return CircularProgressIndicator();
+              }
+            },
+          future: getCurrentWeather(),
+        )
+      ),
     );
   }
 
   Widget weatherBox(Weather _weather) {
-    return Column(children: <Widget>[
-      Text("${_weather.temp}*C"),
-      Text("${_weather.descriptions}"),
-      Text("Feels:${_weather.rFeels}*C"),
-      Text("H:${_weather.high}*C L:${_weather.low}*C"),
-    ]);
+    return Column(
+      children: <Widget>[
+      Container(
+        margin:  const EdgeInsets.all(10),
+        child: Text("${_weather.temp.ceil()}*F",
+        textAlign: TextAlign.center,
+        style: TextStyle(fontWeight: FontWeight.bold, fontSize: 55),),
+      ),
+      Container(
+        margin:  const EdgeInsets.all(10),
+        child: Text("${_weather.descriptions}")
+        ),
+      Container(
+        margin:  const EdgeInsets.all(10),
+        child: Text("Feels:${_weather.rFeels.ceil()}*F")
+        ),
+      Container(
+        margin:  const EdgeInsets.all(10),
+        child: Text("H:${_weather.high.ceil()}*F L:${_weather.low.ceil()}*F")
+        ),
+    ]
+    );
   }
 
   Future getCurrentWeather() async {
-    Weather weather;
+    // Removing Null Safty by using late
+    late Weather weather;
     String city = "St. Louis";
     //apiKey = "9b89af2c88ce9c7ac9a9f6200e249417"
     String apiKey = "9b89af2c88ce9c7ac9a9f6200e249417";
-    var url =
-        "https://api.openwathermap.org/data/2.5/weather?q=$city&appid=$apiKey&units=metric";
+    var url = Uri.https('api.openweathermap.org', '/data/2.5/weather', {
+              'q': city,
+              'appid': apiKey,
+              'units': 'imperial',
+            });
     final response = await http.get(url);
 
     if (response.statusCode == 200) {
       weather = Weather.fromJson(jsonDecode(response.body));
     } else {
       // TODO: Throw Error here
+      print('Request failed with status: ${response.statusCode}.');
     }
 
     return weather;
