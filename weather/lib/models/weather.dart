bcc3111fefe88aeec2c01973a6b3f12ffb242fe2
Anthony Sharp <asharp314@outlook.com>
2023-03-12 22:41:51 -0500

    fixed the non-null double issue

--- weather/lib/models/weather.dart
+++ weather/lib/models/weather.dart
@@ -6,15 +6,21 @@ class Weather {
   final double high;
   final String descriptions;
 
-  Weather({this.temp, this.rFeels, this.low, this.high, this.descriptions});
+  Weather({
+    this.temp = 0.0,
+    this.rFeels = 0.0,
+    this.low = 0.0, 
+    this.high = 0.0,
+    this.descriptions = "",
+    });
 
   factory Weather.fromJson(Map<String, dynamic> json) {
     return Weather(
       temp: json['main']['temp'].toDouble(),
-      rFeels: json['main']['rFeels'].toDouble(),
-      low: json['main']['low'].toDouble(),
-      high: json['main']['high'].toDouble(),
-      descriptions: json['weather'][0]['descriptions'],
+      rFeels: json['main']['feels_like'].toDouble(),
+      low: json['main']['temp_min'].toDouble(),
+      high: json['main']['temp_max'].toDouble(),
+      descriptions: json['weather'][0]['description'],
     );
   }
 }
